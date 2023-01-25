8670a3496d35753d765b397ed111b054ca02cad6
Steve Waldman <swaldman@mchange.com>
2023-01-25 14:17:26 -0500

    Bring MainBlog in as inner class.

--- d24n/src/org/d24n/site/D24nSite.scala
+++ d24n/src/org/d24n/site/D24nSite.scala
@@ -33,8 +33,6 @@ object D24nSite extends Site:
   val serverUrl : Abs    = Abs("https://d24n.org/")
   val basePath  : Rooted = Rooted.root
 
-  val MainBlog        = new D24nTopBlog(this)
-
   val locationSources : immutable.Seq[StaticLocationBindingSource] = immutable.Seq( StaticResources )
   val bindingSources  : immutable.Seq[ZTServerEndpointSource]      = immutable.Seq( MainBlog, StaticResources )
 
@@ -48,110 +46,111 @@ object D24nSite extends Site:
       Vector("wp-content","css","font","image")
         .map( dir => StaticLocationBinding( Rooted.fromElements(dir), JPath.of("d24n/static", dir) ) )
 
-class D24nTopBlog( val site : D24nSite.type ) extends Blog[D24nSite.type,D24nMetadata]:
-  val rawTemplates = IndexedUntemplates.filter { case (fqn, _) => fqn.indexOf(".mainblog.entry") >= 0 }.map( _(1) )
-  val untemplates = rawTemplates.map( _.asInstanceOf[this.Untemplate] ).toVector
+  val MainBlog : Blog[D24nSite.type,D24nMetadata] = new Blog[D24nSite.type,D24nMetadata]:
+    val site = D24nSite.this
+    val rawTemplates = IndexedUntemplates.filter { case (fqn, _) => fqn.indexOf(".mainblog.entry") >= 0 }.map( _(1) )
+    val untemplates = rawTemplates.map( _.asInstanceOf[this.Untemplate] ).toVector
 
-  // reverse-chronological!
-  val resolveds = untemplates.map( ut => Entry.Resolved(ut, entryInfo(ut)) ).to(immutable.SortedSet)
+    // reverse-chronological!
+    val resolveds = untemplates.map( ut => Entry.Resolved(ut, entryInfo(ut)) ).to(immutable.SortedSet)
 
-  def entryInfo( untemplate : this.Untemplate ) =
-    val attrsLc = untemplate.UntemplateAttributes.map { case (k, v) => (k.toLowerCase, v) }
-    def getMaybeMultiple(keySingular : String) : Seq[String] =
-      attrsLc.get(keySingular + "s") match
-        case Some(seq: Seq[_]) => seq.map( _.toString ) // to avoid unchecked Seq[String] match
-        case Some(str: String) => str.split(",").map(_.trim).toSeq
-        case Some( other ) => throw new D24nSite.Exception(s"Unexpected '${keySingular}s' type: ${other}")
-        case None =>
-          attrsLc.get(keySingular) match
-            case Some(str: String) => Seq(str)
-            case Some( other ) => throw new D24nSite.Exception(s"Unexpected '${keySingular}' type: ${other}")
-            case None => Nil
+    def entryInfo( untemplate : this.Untemplate ) =
+      val attrsLc = untemplate.UntemplateAttributes.map { case (k, v) => (k.toLowerCase, v) }
+      def getMaybeMultiple(keySingular : String) : Seq[String] =
+        attrsLc.get(keySingular + "s") match
+          case Some(seq: Seq[_]) => seq.map( _.toString ) // to avoid unchecked Seq[String] match
+          case Some(str: String) => str.split(",").map(_.trim).toSeq
+          case Some( other ) => throw new D24nSite.Exception(s"Unexpected '${keySingular}s' type: ${other}")
+          case None =>
+            attrsLc.get(keySingular) match
+              case Some(str: String) => Seq(str)
+              case Some( other ) => throw new D24nSite.Exception(s"Unexpected '${keySingular}' type: ${other}")
+              case None => Nil
 
-    def parseTimestampIsoInstant( timestamp : String ) : Failable[Instant] =
-      for
-        temporalAccessor <- Failable( ISO_INSTANT.parse(timestamp) )
-      yield
-        Instant.from(temporalAccessor)
+      def parseTimestampIsoInstant( timestamp : String ) : Failable[Instant] =
+        for
+          temporalAccessor <- Failable( ISO_INSTANT.parse(timestamp) )
+        yield
+          Instant.from(temporalAccessor)
 
-    def parseTimestampFromIsoLocalDate( timestamp : String ) : Failable[Instant] =
-      for
-        temporalAccessor <- Failable( ISO_LOCAL_DATE.parse(timestamp)  )
-        ld               <- Failable( LocalDate.from(temporalAccessor) )
-        zdt              <- Failable( ld.atTime(12,0).atZone(ZoneId.systemDefault()) )
-      yield
-        Instant.from(zdt)
+      def parseTimestampFromIsoLocalDate( timestamp : String ) : Failable[Instant] =
+        for
+          temporalAccessor <- Failable( ISO_LOCAL_DATE.parse(timestamp)  )
+          ld               <- Failable( LocalDate.from(temporalAccessor) )
+          zdt              <- Failable( ld.atTime(12,0).atZone(ZoneId.systemDefault()) )
+        yield
+          Instant.from(zdt)
 
-    def parseTimestamp( timestamp : String ) : Instant =
-      (parseTimestampIsoInstant(timestamp) orElseTrace parseTimestampFromIsoLocalDate(timestamp)).assert
+      def parseTimestamp( timestamp : String ) : Instant =
+        (parseTimestampIsoInstant(timestamp) orElseTrace parseTimestampFromIsoLocalDate(timestamp)).assert
 
-    def contentTypeFromSuffix( name : String ) : Option[String] =
-      val suffixDelimiter = name.lastIndexOf("_")
-      if suffixDelimiter >= 0 then
-        val suffix = name.substring(suffixDelimiter + 1)
-        ContentTypeBySuffix.get(suffix)
-      else
-        None
+      def contentTypeFromSuffix( name : String ) : Option[String] =
+        val suffixDelimiter = name.lastIndexOf("_")
+        if suffixDelimiter >= 0 then
+          val suffix = name.substring(suffixDelimiter + 1)
+          ContentTypeBySuffix.get(suffix)
+        else
+          None
 
-    def mediaPathPermalink( pubDate : Instant, title : String ) : (String, String) =
-      val zoned = pubDate.atZone(ZoneId.systemDefault())
-      val year  = zoned.get(ChronoField.YEAR)
-      val month = zoned.get(ChronoField.MONTH_OF_YEAR)
-      val day   = zoned.get(ChronoField.DAY_OF_MONTH)
-      val mediaPath = f"/$year%d/$month%02d/$day%02d/${inLinkTitle(title)}%s/"
-      ( mediaPath, mediaPath + "index.html" )
+      def mediaPathPermalink( pubDate : Instant, title : String ) : (String, String) =
+        val zoned = pubDate.atZone(ZoneId.systemDefault())
+        val year  = zoned.get(ChronoField.YEAR)
+        val month = zoned.get(ChronoField.MONTH_OF_YEAR)
+        val day   = zoned.get(ChronoField.DAY_OF_MONTH)
+        val mediaPath = f"/$year%d/$month%02d/$day%02d/${inLinkTitle(title)}%s/"
+        ( mediaPath, mediaPath + "index.html" )
 
-    val mbTitle = attrsLc.get("title").map( _.toString )
-    val authors = getMaybeMultiple("author")
-    val tags    = getMaybeMultiple("tag")
-    val pubDate =
-      attrsLc.get("pubdate") orElse attrsLc.get("publicationdate") match
-        case Some( instant : Instant )  => instant
-        case Some( timestamp : String ) => parseTimestamp( timestamp.trim )
-        case Some( other )              => throw new D24nSite.Exception(s"Unexpected publication date format: ${other}")
-        case None                       => throw new D24nSite.Exception(s"PubDate or PublicationDate attribute required, not found.")
-    val contentType =
-      (attrsLc.get("content-type").map( _.toString ) orElse contentTypeFromSuffix(untemplate.UntemplateName)).getOrElse("text/plain")
-    val (mediaPathStr, permalinkSiteRootedPathStr) = mediaPathPermalink(pubDate, mbTitle.getOrElse("Untitled Post"))
-    Entry.Info(mbTitle, authors, tags, pubDate, contentType, Rooted(mediaPathStr), Rooted(permalinkSiteRootedPathStr))
+      val mbTitle = attrsLc.get("title").map( _.toString )
+      val authors = getMaybeMultiple("author")
+      val tags    = getMaybeMultiple("tag")
+      val pubDate =
+        attrsLc.get("pubdate") orElse attrsLc.get("publicationdate") match
+          case Some( instant : Instant )  => instant
+          case Some( timestamp : String ) => parseTimestamp( timestamp.trim )
+          case Some( other )              => throw new D24nSite.Exception(s"Unexpected publication date format: ${other}")
+          case None                       => throw new D24nSite.Exception(s"PubDate or PublicationDate attribute required, not found.")
+      val contentType =
+        (attrsLc.get("content-type").map( _.toString ) orElse contentTypeFromSuffix(untemplate.UntemplateName)).getOrElse("text/plain")
+      val (mediaPathStr, permalinkSiteRootedPathStr) = mediaPathPermalink(pubDate, mbTitle.getOrElse("Untitled Post"))
+      Entry.Info(mbTitle, authors, tags, pubDate, contentType, Rooted(mediaPathStr), Rooted(permalinkSiteRootedPathStr))
 
-  private def mainFrame( fragmentText : String ) : String =
-    val mainFrameInput = D24nSite.Frame.Input.Main(fragmentText, site)
-    frame_main_html(mainFrameInput).text
+    private def mainFrame( fragmentText : String ) : String =
+      val mainFrameInput = D24nSite.Frame.Input.Main(fragmentText, site)
+      frame_main_html(mainFrameInput).text
 
-  private def renderSingleFragment( resolved : Entry.Resolved, presentationMultiple : Boolean ) : untemplate.Result[D24nMetadata] =
-    val Entry.Resolved(untemplate, info) = resolved
-    val renderer = ContentRendererForContentType(info.contentType)
-    val entry = Entry(info.mediaPath, presentationMultiple, site)
-    val result = untemplate(entry)
-    val renderResult = renderer(result)
-    val articleFrameInput = D24nSite.Frame.Input.Article(renderResult.text, info.mbTitle, info.authors, info.tags, info.pubDate, site.serverRootedPath(info.permalinkSiteRootedPath), presentationMultiple, site)
-    frame_article_html(articleFrameInput)
+    private def renderSingleFragment( resolved : Entry.Resolved, presentationMultiple : Boolean ) : untemplate.Result[D24nMetadata] =
+      val Entry.Resolved(untemplate, info) = resolved
+      val renderer = ContentRendererForContentType(info.contentType)
+      val entry = Entry(info.mediaPath, presentationMultiple, site)
+      val result = untemplate(entry)
+      val renderResult = renderer(result)
+      val articleFrameInput = D24nSite.Frame.Input.Article(renderResult.text, info.mbTitle, info.authors, info.tags, info.pubDate, site.serverRootedPath(info.permalinkSiteRootedPath), presentationMultiple, site)
+      frame_article_html(articleFrameInput)
 
-  def renderSingle( resolved : Entry.Resolved, presentationMultiple : Boolean ) : String =
-    val articleResult = renderSingleFragment( resolved, presentationMultiple )
-    mainFrame( articleResult.text )
+    def renderSingle( resolved : Entry.Resolved, presentationMultiple : Boolean ) : String =
+      val articleResult = renderSingleFragment( resolved, presentationMultiple )
+      mainFrame( articleResult.text )
 
-  private def renderResolveds( ssr : immutable.SortedSet[Entry.Resolved] ) : String =
-    val fragmentTexts = ssr.to(List).map(resolved => renderSingleFragment(resolved, true).text)
-    val unifiedFragmentText = fragmentTexts.mkString(decorative.article_separator_html().text)
-    mainFrame(unifiedFragmentText)
+    private def renderResolveds( ssr : immutable.SortedSet[Entry.Resolved] ) : String =
+      val fragmentTexts = ssr.to(List).map(resolved => renderSingleFragment(resolved, true).text)
+      val unifiedFragmentText = fragmentTexts.mkString(decorative.article_separator_html().text)
+      mainFrame(unifiedFragmentText)
 
-  def renderLast( num : Int ) : String =
-    val rs = resolveds.take(num)
-    renderResolveds( rs )
+    def renderLast( num : Int ) : String =
+      val rs = resolveds.take(num)
+      renderResolveds( rs )
 
-  def renderRange( from : Instant, until : Instant ) : String =
-    val ordering = summon[Ordering[Instant]]
-    val rs = resolveds.filter( r => ordering.gteq(from,r.info.pubDate) && ordering.lt(r.info.pubDate, until) )
-    renderResolveds( rs )
+    def renderRange( from : Instant, until : Instant ) : String =
+      val ordering = summon[Ordering[Instant]]
+      val rs = resolveds.filter( r => ordering.gteq(from,r.info.pubDate) && ordering.lt(r.info.pubDate, until) )
+      renderResolveds( rs )
 
-  def endpointBindings : immutable.Seq[ZTEndpointBinding] =
-    val permalinks =  resolveds.to(Vector)
-      .map { r =>
-        publicReadOnlyHtmlEndpointBinding(r.info.permalinkSiteRootedPath, site, ZIO.attempt( renderSingle(r, false)))
-      }
-    permalinks :+ publicReadOnlyHtmlEndpointBinding( Rooted.root, site, ZIO.attempt( renderLast(10) ) )
+    def endpointBindings : immutable.Seq[ZTEndpointBinding] =
+      val permalinks =  resolveds.to(Vector)
+        .map { r =>
+          publicReadOnlyHtmlEndpointBinding(r.info.permalinkSiteRootedPath, site, ZIO.attempt( renderSingle(r, false)))
+        }
+      permalinks :+ publicReadOnlyHtmlEndpointBinding( Rooted.root, site, ZIO.attempt( renderLast(10) ) )
 
 
 
